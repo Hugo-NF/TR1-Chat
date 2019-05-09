e9b6cdb4c7c7f5f35cc1da044cade7569e972e35
Hugo-NF <hugonfonseca@hotmail.com>
2019-05-08 22:15:31 -0300

    Initial project structure

---
+++ src/client.c
@@ -0,0 +1,13 @@
+// Compiler includes
+#include <stdlib.h>
+#include <unistd.h>
+
+// Project includes
+#include "../include/console.h"
+
+int main(int argc, char **argv) {
+
+
+    return 0;
+
+}
---
+++ src/server.c
@@ -0,0 +1,13 @@
+// Compiler includes
+#include <sqlite3.h>
+
+
+// Project includes
+#include "../include/console.h"
+#include "../include/settings.h"
+
+int main(int argc, char **argv) {
+
+
+    return 0;
+}
