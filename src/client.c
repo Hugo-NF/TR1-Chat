ac4160914521c8032f2daf7b7ca66d8b30ab7c3f
Hugo-NF <hugonfonseca@hotmail.com>
2019-05-12 17:49:41 -0300

    Initial server structure

--- src/client.c
+++ src/client.c
@@ -1,12 +1,29 @@
 // Compiler includes
+#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 // Project includes
-#include "../include/console.h"
+#include "client.h"
+#include "console.h"
+#include "settings.h"
 
 int main(int argc, char **argv) {
 
+//    msg send, rcv;
+//
+//    int queue_id = msgget(NETWORK_KEY, 0x1FF);
+//    if(queue_id < 0){
+//        error("Could not connect to server\n");
+//        exit(1);
+//    }
+//    msgrcv(queue_id, &rcv, sizeof(msg), 2, 0);
+//
+//    message("Client PID: %d\nMsg addr: %d\nCode: %d\n", getpid(), rcv.mtype, rcv.opcode);
+//
+//    send.mtype = 1;
+//    send.opcode = -31;
+//    msgsnd(queue_id, &send, sizeof(msg), 0);
 
     return 0;
 
