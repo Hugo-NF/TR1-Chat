59cb1838a2c685dd568e1d7e75094b1c272578e5
Hugo-NF <hugonfonseca@hotmail.com>
2019-05-28 09:43:44 -0300

    Initial commit since new project implementation. Initial structure of server and client classes

---
+++ src/client.py
@@ -0,0 +1,19 @@
+# Project imports
+# Python built-in for sockets programming
+from socket import AF_INET, socket, SOCK_STREAM
+
+# Python build-in for handling threads
+from threading import Thread
+
+class Client:
+    """Handles the communication with the server application"""
+
+    def __init__(self, host, port):
+        self.conn_host = host
+        self.conn_port = port
+        self.conn_address = (self.conn_host, self.conn_port)
+        self.socket = socket(AF_INET, SOCK_STREAM)
+
+    def connect(self):
+        self.socket.connect(self.conn_address)
+
---
+++ src/server.py
@@ -0,0 +1,22 @@
+# Project imports
+# Python built-in for sockets programming
+from socket import AF_INET, socket, SOCK_STREAM
+
+# Python build-in for handling threads
+from threading import Thread
+
+class Server:
+    """Implements a multithreaded server for a asynchronous chat application"""
+
+    buffer_size = 1024
+
+    def __init__(self, host, port):
+        self.clients = {}
+        self.addresses = {}
+        self.host = host
+        self.port = port
+        self.own_address = (self.host, self.port)
+        self.socket = socket(AF_INET, SOCK_STREAM)
+
+    def start_server(self):
+        self.socket.bind(self.own_address)
